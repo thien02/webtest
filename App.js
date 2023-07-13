61d6b22cb3b67017fb7861061f6fecad92bba775
thien02 <106140494+thien02@users.noreply.github.com>
2023-07-13 19:14:24 +0700

    lan1

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {Navbar} from "./component/navbar"
+import { Shop } from "./page/shop/shop";
+import { Contact } from "./page/contact";
+import { Cart } from "./page/cart/cart";
+
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
